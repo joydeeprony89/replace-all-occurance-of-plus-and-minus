80e0f67400d8db7e84ba5a3290a1cc8cd0fb1b9c
jd_786 <jaydeep_shil@certree.com>
2022-02-09 12:14:01 +0530

    Add project files.

---
+++ replace all occurance of plus and minus/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace replace_all_occurance_of_plus_and_minus
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Program p = new Program();
+      Console.WriteLine(p.solution("plusminusminusplus", "plus", "minus"));
+    }
+
+    public string solution(string S, string plus, string minus)
+    {
+      // Replacing all occurrences of 
+      // Sub in Str by empty spaces 
+      S = S.Replace(plus, "+");
+      S = S.Replace(minus, "-");
+
+      // Removing unwanted spaces in the 
+      // start and end of the string 
+      S = S.Trim();
+
+      return S;
+    }
+  }
+}
